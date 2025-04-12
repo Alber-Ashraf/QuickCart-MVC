04bfd7e3e1d6a2b765329c12e6a9199040cc6e07
BeRo <BeRo@BERO>
2025-04-12 12:19:18 +0200

    Configuring DbContext for QuickCart

---
+++ QuickCart/Data/QuickCartDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuickCart.Data
+{
+    public class QuickCartDbContext : DbContext
+    {
+        public QuickCartDbContext(DbContextOptions<QuickCartDbContext> options) : base(options)
+        {
+        }
+    }
+}
