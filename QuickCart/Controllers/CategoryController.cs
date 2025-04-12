cc24fe011c4c3554ce8a8c5e3f1de3a4877d1a7d
BeRo <BeRo@BERO>
2025-04-12 17:43:12 +0200

    Get all Category data

--- QuickCart/Controllers/CategoryController.cs
+++ QuickCart/Controllers/CategoryController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuickCart.Data;
+using QuickCart.Models;
 
 namespace QuickCart.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly QuickCartDbContext _db;
+
+        public CategoryController(QuickCartDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Fetch the list of categories from the database
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            // Pass the list to the View
+            return View(objCategoryList);
         }
     }
 }
