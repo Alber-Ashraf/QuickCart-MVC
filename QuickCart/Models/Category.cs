1d1dd58db42657f57887e50f75b301ee37c09b35
BeRo <BeRo@BERO>
2025-04-13 13:21:16 +0200

    Add Validation for Category

--- QuickCart/Models/Category.cs
+++ QuickCart/Models/Category.cs
@@ -7,9 +7,13 @@ namespace QuickCart.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [Range(1, 100)]
         [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
