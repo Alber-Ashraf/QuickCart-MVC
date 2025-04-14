f240bd5772ee18290432b3b2c7d1d7f21e0efe52
BeRo <BeRo@BERO>
2025-04-14 15:51:40 +0200

    Add splash screen

--- QuickCart/wwwroot/js/site.js
+++ QuickCart/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿window.addEventListener("load", function () {
+    setTimeout(function () {
+        document.body.classList.add("loaded");
+    }, 3000); // Delay for 3 seconds
+});
