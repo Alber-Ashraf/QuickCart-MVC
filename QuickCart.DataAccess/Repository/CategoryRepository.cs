f4f240ba39dd74701d70c230ec8eda6e3ab1a76a
BeRo <BeRo@BERO>
2025-04-25 12:39:33 +0300

    Add Category Repository

---
+++ QuickCart.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickCart.Data;
+using QuickCart.DataAccess.Repository.IRepository;
+using QuickCart.Models;
+
+namespace QuickCart.DataAccess.Repository
+{
+    class CategoryRepository : Repository<Category>, ICategoryRepository 
+    {
+        private readonly QuickCartDbContext _db;
+        public CategoryRepository(QuickCartDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category obj)
+        {   
+            _db.Categories.Update(obj);
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ QuickCart.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickCart.Models;
+
+namespace QuickCart.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        // Define methods specific to Category repository
+        void Update(Category obj);
+        void Save();
+    }
+}
