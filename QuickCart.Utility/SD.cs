e6502014108f2db2fb381420885da9528567ed96
BeRo <BeRo@BERO>
2025-05-03 15:21:29 +0300

    Update SD Class

--- QuickCart.Utility/SD.cs
+++ QuickCart.Utility/SD.cs
@@ -12,5 +12,17 @@ namespace QuickCart.Utility
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
         public const string Role_Company = "Company";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Progressing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
     }
 }
