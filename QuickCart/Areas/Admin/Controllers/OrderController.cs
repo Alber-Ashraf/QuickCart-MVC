c021a25da8e172f49c29ba9704ed4659918cb450
BeRo <BeRo@BERO>
2025-05-04 15:29:53 +0300

    Add Order Controller and View Model

---
+++ QuickCart/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using QuickCart.DataAccess.Repository.IRepository;
+using QuickCart.Models;
+
+namespace QuickCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // This method is used to get the list of Products in JSON format for DataTables
+        #region ApI Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Fetch the list of Products from the database
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includedProperties: "ApplicationUser").ToList();
+            // Return the list as JSON
+            return Json(new { data = objOrderHeaders });
+        }
+
+        #endregion
+    }
+}
