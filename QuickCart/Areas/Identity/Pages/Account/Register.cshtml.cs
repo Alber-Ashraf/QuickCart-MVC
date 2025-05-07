f85d5af45b22b965bb6059ca905e63abfe9855f2
BeRo <BeRo@BERO>
2025-05-07 16:41:50 +0300

    Update Register Authentication

--- QuickCart/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ QuickCart/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -210,7 +210,14 @@ namespace QuickCart.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["Success"] = "User created successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
