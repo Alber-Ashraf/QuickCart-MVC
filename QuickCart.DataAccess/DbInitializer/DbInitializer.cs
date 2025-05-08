ff44cfcf0aaa0176a50fc7c5218bb96c1a9d8eba
BeRo <BeRo@BERO>
2025-05-08 15:00:32 +0300

    Azure Production Deployment

---
+++ QuickCart.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using QuickCart.Data;
+using QuickCart.Models;
+using QuickCart.Utility;
+
+namespace QuickCart.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly QuickCartDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public DbInitializer(QuickCartDbContext db , UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public void Initialize()
+        {
+            // Apply any pending migrations
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            // Seed the database with initial data
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+                // Create default admin user
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "alberAdmin@gmail.com",
+                    Email = "alberAdmin@gmail.com",
+                    Name = "Alber",
+                    PhoneNumber = "01228342430",
+                    Address = "123 Main St",
+                    City = "New York",
+                    State = "NY",
+                    PostalCode = "10001",
+                }, "139BeRo!").GetAwaiter().GetResult();
+
+                // Assign the admin role to the default admin user
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "alberAdmin@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+
+            return;
+        }
+    }
+}
+    
--- QuickCart/Program.cs
+++ QuickCart/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using QuickCart.Utility;
 using Stripe;
 using Microsoft.AspNetCore.Authentication.OAuth;
+using QuickCart.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -50,6 +51,8 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
@@ -74,9 +77,20 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+SeedDatabase();
+
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
