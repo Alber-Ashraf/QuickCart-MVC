39b26414cab839dc2974035a220738f991384fcd
BeRo <BeRo@BERO>
2025-05-09 01:25:27 +0300

    Add Permission Management Action and View

---
+++ QuickCart.Models/ViewModels/UserVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace QuickCart.Models.ViewModels
+{
+    public class UserVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> Company_List { get; set; }
+        public IEnumerable<SelectListItem> Role_List { get; set; }
+    }
+}
--- QuickCart/Areas/Admin/Controllers/UserController.cs
+++ QuickCart/Areas/Admin/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using QuickCart.Data;
+using QuickCart.DataAccess.Repository;
 using QuickCart.DataAccess.Repository.IRepository;
 using QuickCart.Models;
 using QuickCart.Models.ViewModels;
@@ -12,7 +14,7 @@ using QuickCart.Utility;
 namespace QuickCart.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+    [Authorize(Roles = SD.Role_Admin)]
     public class UserController : Controller
     {
         private readonly QuickCartDbContext _db;
@@ -27,7 +29,93 @@ namespace QuickCart.Areas.Admin.Controllers
             // Pass the list to the View
             return View();
         }
-        
+
+        public IActionResult ManagePermission(string? id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var userFromDb = _db.ApplicationUsers
+                .Include(u => u.Company)
+                .FirstOrDefault(u => u.Id == id);
+
+            if (userFromDb == null)
+            {
+                return NotFound();
+            }
+
+            // Get role name directly from joins
+            var userRole = (from userRoles in _db.UserRoles
+                            join roles in _db.Roles on userRoles.RoleId equals roles.Id
+                            where userRoles.UserId == id
+                            select roles.Name).FirstOrDefault();
+
+            userFromDb.Role = userRole;
+
+            UserVM userVM = new()
+            {
+                ApplicationUser = userFromDb,
+
+                Role_List = _db.Roles.Select(role => new SelectListItem
+                {
+                    Text = role.Name,
+                    Value = role.Name
+                }),
+
+                Company_List = _db.Companies.Select(company => new SelectListItem
+                {
+                    Text = company.Name,
+                    Value = company.Id.ToString()
+                })
+            };
+
+            return View(userVM);
+        }
+
+        [HttpPost]
+        public IActionResult ManagePermission(UserVM userVM)
+        {
+            var userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == userVM.ApplicationUser.Id);
+            if (userFromDb == null)
+            {
+                return NotFound();
+            }
+
+            // 1. Update CompanyId
+            userFromDb.CompanyId = userVM.ApplicationUser.CompanyId;
+
+            // 2. Remove existing role
+            var userRole = _db.UserRoles.FirstOrDefault(x => x.UserId == userVM.ApplicationUser.Id);
+            if (userRole != null)
+            {
+                _db.UserRoles.Remove(userRole);
+            }
+
+            // 3. Assign new role
+            var newRole = _db.Roles.FirstOrDefault(x => x.Name == userVM.ApplicationUser.Role);
+            if (newRole != null)
+            {
+                _db.UserRoles.Add(new IdentityUserRole<string>
+                {
+                    UserId = userFromDb.Id,
+                    RoleId = newRole.Id
+                });
+            }
+
+            _db.SaveChanges();
+            // 4. Update the user in the database
+            _db.ApplicationUsers.Update(userFromDb);
+
+            // 4. Show success message
+
+            TempData["success"] = "User permissions updated successfully.";
+            return RedirectToAction("Index");
+        }
+
+
+
         // This method is used to get the list of Companys in JSON format for DataTables
         #region ApI Calls
         [HttpGet]
