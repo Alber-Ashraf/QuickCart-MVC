62b980a7643cc704a1fa5f038bc45ecd42869d06
BeRo <BeRo@BERO>
2025-05-08 22:17:09 +0300

    Add User Controller and View

---
+++ QuickCart/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuickCart.Data;
+using QuickCart.DataAccess.Repository.IRepository;
+using QuickCart.Models;
+using QuickCart.Models.ViewModels;
+using QuickCart.Utility;
+
+namespace QuickCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+    public class UserController : Controller
+    {
+        private readonly QuickCartDbContext _db;
+
+        public UserController(QuickCartDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // Pass the list to the View
+            return View();
+        }
+        
+        // This method is used to get the list of Companys in JSON format for DataTables
+        #region ApI Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Fetch the list of Companys from the database
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(c=>c.Company).ToList();
+
+            foreach (var user in objUserList)
+            {
+                if (user.Company == null)
+                {
+                    // If the Company is not null, set the Company name
+                    user.Company = new Company
+                    {
+                        Name = ""
+                    };
+                }
+
+            }
+
+            // Return the list as JSON
+            return Json(new { data = objUserList });
+        }
+
+        // This method is used to get the details of a specific Company for editing
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { sucess = true, massege = "Delete Sucsess" });
+        }
+        #endregion
+
+    }
+}
+
---
+++ QuickCart/wwwroot/js/user.js
@@ -0,0 +1,59 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/Admin/User/GetAll',
+            dataSrc: 'data'
+        },
+        "columns": [
+            {
+                data: 'name',
+                width: "20%",
+                className: "text-nowrap"
+            },
+            {
+                data: 'email',
+                width: "20%",
+                className: "text-nowrap"
+            },
+            {
+                data: 'phoneNumber',
+                width: "10%",
+                className: "text-nowrap"
+            },
+            {
+                data: 'company.name',
+                width: "10%",
+                className: "text-nowrap"
+            },
+            {
+                data: '',
+                width: "15%",
+                className: "text-nowrap"
+            },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `
+                    <div class="btn-group w-100 justify-content-center" role="group">
+                        <a href="/Admin/User/Edit?id=${data}"
+                           class="btn btn-dark mx-2"> <i class="bi bi-pencil-square"></i> Edit
+                        </a>
+                    </div>`;
+                },
+                "width": "25%",
+                className: "text-nowrap"
+            }
+        ],
+        responsive: true,
+        language: {
+            emptyTable: "No users available",
+            search: "_INPUT_",
+            searchPlaceholder: "Search users..."
+        }
+    });
+}
